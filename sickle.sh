3c193409e6f7c8ee88cd0a9ff70ecb80855c2074
dickgroenenberg <dickgroenenberg@gmail.com>
2022-11-09 15:16:16 +0100

    changed outlocation; sanity check added

--- sickle.sh
+++ sickle.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-#outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
-outlocation=$(mktemp -d /home/galaxy/galaxy/database/XXXXXX)
 
+outlocation=$(mktemp -d /data/files/XXXXXX)
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+
+# sanity check
+printf "Conda env: $CONDA_DEFAULT_ENV\n"
+printf "Outlocation: $outlocation\n"
+printf "Python version: $(python --version |  awk '{print $2}')\n"
+printf "Biopython version: $(conda list | egrep biopython | awk '{print $2}')\n"
+printf "Pandas version: $(conda list | egrep sickle-trim | awk '{print $2}')\n"
+printf "Unzip version: $(unzip -v | head -n1 | awk '{print $2}')\n"
+printf "Bash version: ${BASH_VERSION}\n"
+printf "SCRIPTDIR: $SCRIPTDIR\n\n"
 
 python $SCRIPTDIR"/sickle_wrapper.py" -i $1 -of $outlocation -t $4 -q $5 -l $6
 mv $outlocation"/log.log" $3
